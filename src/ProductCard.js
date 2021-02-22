9558c03a4f7d2c47c1b08117864081da934213a9
Miriam <miriam.haenle@gmail.com>
2021-02-22 17:43:59 +0100

    Update design for product card

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,18 +1,25 @@
+import styled from 'styled-components';
+
 export default function ProductCard({ product }) {
-  console.log(product);
   return (
-    <>
+    <Card>
       <h4>{product.name}</h4>
-      <span>{product.price}</span>
-      <span>{product.currency}</span>
-      <span>{product.packageSize}</span>
-      <span>{product.supportContact}</span>
       <div>
-        {product.tags.map((tag) => (
-          <span>{tag}</span>
-        ))}
+        <span>{product.price} </span>
+        <span>{product.currency}</span>
       </div>
-      <span>{product.tag}</span>
-    </>
+      <div>Package size: {product.packageSize}</div>
+      <div>Contact: {product.supportContact}</div>
+
+      <ul>{product.tag}</ul>
+    </Card>
   );
 }
+
+const Card = styled.div`
+  background: #e3c5bb;
+  border-radius: 5px;
+  max-width: 500px;
+  margin: 1rem auto;
+  padding: 1.5rem;
+`;
