45b8a68b9749985e71c4eb1c4995ea676771f4d3
Miriam <miriam.haenle@gmail.com>
2021-02-26 17:06:02 +0100

    Add router

---
+++ src/pages/Favorites.js
@@ -0,0 +1,3 @@
+export default function Favorites() {
+  return <h1>Favorite Products</h1>;
+}
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,21 @@
-import Button from '../Button';
+import ProductForm from '../components/ProductForm';
+import ProductCard from '../components/ProductCard';
 
 export default function Products() {
-  return <Button text="Add a new product" />;
+  return;
+  <>
+    <h1>Products</h1>;
+    <ProductForm onSubmitForm={addProduct} />
+    {products?.map((product) => (
+      <ProductCard
+        product={product}
+        key={product.id}
+        onDeleteCard={() => deleteCard(product.id)}
+        onAddToFavorites={() => addFavoriteProduct(product)}
+        isFavorite={favoriteProducts.some(
+          (favoriteProduct) => product.id === favoriteProduct.id
+        )}
+      />
+    ))}
+  </>;
 }
